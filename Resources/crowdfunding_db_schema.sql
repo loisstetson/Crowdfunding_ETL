549aad05910b1a94f588b23450642da29a6a2d13
Lois Stetson <127718619+loisstetson@users.noreply.github.com>
2023-09-12 17:13:23 -0500

    recommit schema

--- Resources/crowdfunding_db_schema.sql
+++ Resources/crowdfunding_db_schema.sql
@@ -1,75 +1,67 @@
+-- Create the 'campaign' table
 CREATE TABLE "campaign" (
-    "cf_id" int   NOT NULL,
-    "contact_id" int   NOT NULL,
-    "company_name" varchar(100)   NOT NULL,
-    "description" text   NOT NULL,
-    "goal" numeric(10,2)   NOT NULL,
-    "pledged" numeric(10,2)   NOT NULL,
-    "outcome" varchar(50)   NOT NULL,
-    "backers_count" int   NOT NULL,
-    "country" varchar(10)   NOT NULL,
-    "currency" varchar(10)   NOT NULL,
-    "launch_date" date   NOT NULL,
-    "end_date" date   NOT NULL,
-    "category_id" varchar(10)   NOT NULL,
-    "subcategory_id" varchar(10)   NOT NULL,
-    CONSTRAINT "pk_campaign" PRIMARY KEY (
-        "cf_id"
-     )
+    "cf_id" int NOT NULL,
+    "contact_id" int NOT NULL,
+    "company_name" varchar(100) NOT NULL,
+    "description" text NOT NULL,
+    "goal" numeric(10,2) NOT NULL,
+    "pledged" numeric(10,2) NOT NULL,
+    "outcome" varchar(50) NOT NULL,
+    "backers_count" int NOT NULL,
+    "country" varchar(10) NOT NULL,
+    "currency" varchar(10) NOT NULL,
+    "launch_date" date NOT NULL,
+    "end_date" date NOT NULL,
+    "category_id" varchar(10) NOT NULL,
+    "subcategory_id" varchar(10) NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY ("cf_id")
 );
 
+-- Create the 'backers' table
 CREATE TABLE "backers" (
-    "backer_id" varchar(10)   NOT NULL,
-    "cf_id" int   NOT NULL,
-    "first_name" varchar(50)   NOT NULL,
-    "last_name" varchar(50)   NOT NULL,
-    "email" varchar(100)   NOT NULL,
-    CONSTRAINT "pk_backers" PRIMARY KEY (
-        "backer_id"
-     )
+    "backer_id" varchar(10) NOT NULL,
+    "cf_id" int NOT NULL,
+    "first_name" varchar(50) NOT NULL,
+    "last_name" varchar(50) NOT NULL,
+    "email" varchar(100) NOT NULL,
+    CONSTRAINT "pk_backers" PRIMARY KEY ("backer_id")
 );
 
+-- Create the 'contacts' table
 CREATE TABLE "contacts" (
-    "contact_id" int   NOT NULL,
-    "first_name" varchar(50)   NOT NULL,
-    "last_name" varchar(50)   NOT NULL,
-    "email" varchar(100)   NOT NULL,
-    CONSTRAINT "pk_contacts" PRIMARY KEY (
-        "contact_id"
-     )
+    "contact_id" int NOT NULL,
+    "first_name" varchar(50) NOT NULL,
+    "last_name" varchar(50) NOT NULL,
+    "email" varchar(100) NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY ("contact_id")
 );
 
+-- Create the 'category' table
 CREATE TABLE "category" (
-    "category_id" varchar(10)   NOT NULL,
-    "category_name" varchar(50)   NOT NULL,
-    CONSTRAINT "pk_category" PRIMARY KEY (
-        "category_id"
-     )
+    "category_id" varchar(10) NOT NULL,
+    "category_name" varchar(50) NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY ("category_id")
 );
 
+-- Create the 'subcategory' table
 CREATE TABLE "subcategory" (
-    "subcategory_id" varchar(10)   NOT NULL,
-    "subcategory_name" varchar(50)   NOT NULL,
-    CONSTRAINT "pk_subcategory" PRIMARY KEY (
-        "subcategory_id"
-     )
+    "subcategory_id" varchar(10) NOT NULL,
+    "subcategory_name" varchar(50) NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY ("subcategory_id")
 );
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+-- Add foreign key constraints
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY ("contact_id")
 REFERENCES "contacts" ("contact_id");
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY ("category_id")
 REFERENCES "category" ("category_id");
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY ("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
 
-ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
+ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY ("cf_id")
 REFERENCES "campaign" ("cf_id");
 
-SELECT * from backers;
-
-
-
-ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
-REFERENCES "campaign" ("cf_id");
+-- Select all records from the 'backers' table
+SELECT * FROM backers;
